195b7f5898feaa20b126d601883b30744429135e
MadelayneM <madelayne075@gmail.com>
2026-09-11 22:08:41 -0300

    Início do projeto Quiz Bandeiras

---
+++ Quiz Bandeiras/app/src/main/java/com/example/projeto_quiz/Pergunta1.java
@@ -0,0 +1,30 @@
+package com.example.projeto_quiz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioGroup;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Pergunta1 extends AppCompatActivity {
+
+    private RadioGroup rgpAlternativas;
+    private Button bntResponder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_pergunta1);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
